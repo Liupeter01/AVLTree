c28c6ffb38c8a450f35c953c97f6b507eba9b71a
lph <liupeter012001@live.cn>
2020-11-26 21:14:10 +0800

    更新RR旋转算法

--- 二叉平衡树AVL/AVLRotate.c
+++ 二叉平衡树AVL/AVLRotate.c
@@ -16,7 +16,11 @@ void AVLTreeRotateLL(AVLNode** parent)					//单旋LL旋转，修改指针用二级指针
 
 void AVLTreeRotateRR(AVLNode** parent)					//单旋RR旋转，修改指针用二级指针
 {
-
+		  AVLNode* SubLeft = *parent;						//新左子树
+		  *parent = SubLeft->rchild;						    //新根节点
+		  SubLeft->rchild = (*parent)->lchild;		   //可能新根结点存在左子树
+		  (*parent)->lchild = SubLeft;						     //将新左子树作为新根节点的左孩子
+		  (*parent)->BF = SubLeft->BF = 0;					//修改BF平衡因子
 }
 
 void AVLTreeRotateRL(AVLNode** parent)						//双旋RL旋转，修改指针用二级指针
